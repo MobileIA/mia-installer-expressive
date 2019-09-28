2521a1ab2b905922b2878f2dad83cd8e8620015d
Matias Camiletti <matias.camiletti@gmail.com>
2019-09-27 23:56:38 -0300

    Agregado remove handler.

---
+++ src/Generate/RemoveHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Mobileia\Expressive\Installer\Generate;
+
+class RemoveHandler extends BaseFile
+{
+    /**
+     * Path del archivo a tener de base
+     * @var string
+     */
+    protected $filePath = './vendor/mobileia/mia-installer-expressive/data/g_handler_remove.txt';
+    /**
+     * Path de la carpeta donde se va a guardar
+     * @var string
+     */
+    protected $savePath = './src/App/src/Handler/';
+    /**
+     * Nombre de la DB
+     *
+     * @var string
+     */
+    public $name = '';
+
+    public function run()
+    {
+        $this->file = str_replace('%%nameClass%%', $this->getCamelCase($this->name), $this->file);
+        $this->file = str_replace('%%name%%', $this->name, $this->file);
+        
+        try {
+            mkdir($this->savePath . '/' . $this->getCamelCase($this->name), 0777, true);
+        } catch (\Exception $exc) { }
+        file_put_contents($this->savePath . '/' . $this->getCamelCase($this->name) . '/RemoveHandler.php', $this->file);
+    }
+}
