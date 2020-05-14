62d6f48fab494e4f8c3414cba56ce30b540d5677
Matias Camiletti <matias.camiletti@gmail.com>
2020-05-14 11:20:16 -0300

    Unificado creacion de screen.

--- src/Generate/Flutter/Screen.php
+++ src/Generate/Flutter/Screen.php
@@ -10,16 +10,6 @@ namespace Mobileia\Expressive\Installer\Generate\Flutter;
 class Screen extends \Mobileia\Expressive\Installer\Generate\BaseFile
 {
     /**
-     * Path del archivo a tener de base
-     * @var string
-     */
-    protected $filePath = './vendor/mobileia/mia-installer-expressive/data/flutter/view.txt';
-    /**
-     * Path de la carpeta donde se va a guardar
-     * @var string
-     */
-    protected $savePath = './data/flutter/screens/';
-    /**
      * Nombre de la DB
      *
      * @var string
@@ -28,12 +18,17 @@ class Screen extends \Mobileia\Expressive\Installer\Generate\BaseFile
 
     public function run()
     {
-        $this->file = str_replace('%%nameClass%%', $this->getCamelCase($this->name), $this->file);
-        $this->file = str_replace('%%name%%', $this->name, $this->file);
+        $model = new Controller();
+        $model->name = $this->name;
+        $model->run();
+
+        $model = new View();
+        $model->name = $this->name;
+        $model->run();
+    }
+
+    protected function openFile()
+    {
         
-        try {
-            mkdir($this->savePath . $this->name . '/', 0777, true);
-        } catch (\Exception $exc) { }
-        file_put_contents($this->savePath . $this->name  . '/' . $this->name . '.view.dart', $this->file);
     }
 }
